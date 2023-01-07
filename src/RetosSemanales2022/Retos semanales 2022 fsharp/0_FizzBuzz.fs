fb4dc6f984a793f668fd4b37267f8baa582ef4df
Albert Capdevila <albert.capdevila@buclenet.com>
2023-01-07 23:51:31 +0100

    Enunciado Reto 0 FizzBuzz para f#

--- src/RetosSemanales2022/Retos semanales 2022 fsharp/0_FizzBuzz.fs
+++ src/RetosSemanales2022/Retos semanales 2022 fsharp/0_FizzBuzz.fs
@@ -1,3 +1,22 @@
+﻿(*
+ * Reto #0
+ * EL FAMOSO "FIZZ BUZZ"
+ * Fecha publicación enunciado: 27/12/21
+ * Fecha publicación resolución: 07/01/23
+ * Dificultad: FÁCIL
+ * Enunciado: Escribe un programa que muestre por consola (con un print) los números de 1 a 100 (ambos incluidos y con un salto de línea entre cada impresión), sustituyendo los siguientes:
+ * - Múltiplos de 3 por la palabra "fizz".
+ * - Múltiplos de 5 por la palabra "buzz".
+ * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
+ *
+ * Información adicional:
+ * - Usa el canal de nuestro discord (https://mouredev.com/discord) "🔁reto-semanal" para preguntas, dudas o prestar ayuda a la acomunidad.
+ * - Puedes hacer un Fork del repo y una Pull Request al repo original para que veamos tu solución aportada.
+ * - Revisaré el ejercicio en directo desde Twitch el lunes siguiente al de su publicación.
+ * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
+ *
+ *)
+
 module Reto0
 
 open Xunit
